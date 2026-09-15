82faa4751eea5dc91131c5327c6ff8e47ce0c6a6
Jitesh Harchandani <jiteshharchandani3@gmail.com>
2026-09-15 19:55:44 +0530

    Fix API module format

--- api/contact.js
+++ api/contact.js
@@ -174,9 +174,9 @@
 //     });
 //   }
 // };
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.status(200).json({
     success: true,
     message: "API is working"
   });
-}
+};
